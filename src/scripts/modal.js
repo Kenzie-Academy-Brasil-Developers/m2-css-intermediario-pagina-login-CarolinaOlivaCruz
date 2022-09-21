d9c0c48f0d0e69c246745393d70cd746e4168ab1
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2022-09-21 18:05:06 -0300

    add

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -6,29 +6,68 @@
 // abra um modal para criar nova senha;
 // após criar nova senha voltar para a página de login;
 
+function createLoginModal() {
+    const modalContainer = document.createElement('section')
+    const contentContainer = document.createElement('div')
+    const buttonContainer = document.createElement('div')
+    const closeModal = document.createElement('button')
+    const formContainer = document.createElement('form')
+    const formTitle = document.createElement('h2')
+    const passwordInput1 = document.createElement('input')
+    const passwordInput2 = document.createElement('input')
+    const submitButton = document.createElement('button')
+    const message = document.createElement('p')
 
-function rendrizaNovaSenha() {
+    modalContainer.id = 'modal_container'
 
-    const p = document.querySelector("#new-password");
-    const footer = document.querySelector(".footer");
-    const form = document.createElement("form");
-    const h2 = document.createElement("h2");
-    const input1 = document.createElement("input");
-    const input2 = document.createElement("input");
-    const button = document.createElement("button");
+    closeModal.id = 'close_modal'
+    closeModal.innerText = 'X'
 
-    h2.innerText = "Crie a nova senha";
-    input1.placeholder = "Digite sua nova senha";
-    input2.placeholder = "Digite sua nova senha novamente";
-    button.innerText = "Salvar";
+    formTitle.innerText = 'Login'
 
-    // if (input1.value === input2) {
-    //     button.addEventListener("click",);
-    // } 
+    passwordInput1.placeholder = 'Digite sua nova senha'
+    passwordInput1.type = 'password'
+    passwordInput1.classList = 'input-value'
 
-    // p.addEventListener("click", )
-    form.append(h2, input1, input2, button);
-    footer.appendChild(form);
+    passwordInput2.placeholder = 'Digite sua nova senha novamente'
+    passwordInput2.type = 'password'
+    passwordInput2.classList = 'input-value'
+
+    submitButton.innerText = 'Salvar'
+    submitButton.classList = 'input-button'
+
+    buttonContainer.append(closeModal)
+    formContainer.append(formTitle, passwordInput1, passwordInput2, submitButton, message)
+    contentContainer.append(buttonContainer, formContainer)
+    modalContainer.append(contentContainer)
+    
+
+    return modalContainer
 }
-rendrizaNovaSenha();
+
+function showModal() {
+    const modalButton = document.getElementById('new-password')
+    const mainContainer = document.getElementById('main_container')
+
+    modalButton.addEventListener('click', (event) => {
+        event.preventDefault()
+        const modal = createLoginModal()
+
+        mainContainer.appendChild(modal)
+
+        closeModal()
+    })
+}
+
+function closeModal() {
+    const closeModalButton = document.getElementById('close_modal')
+    const modalContainer = document.getElementById('modal_container')
+
+    closeModalButton.addEventListener('click', () => {
+        modalContainer.remove()
+    })
+}
+
+showModal()
+
 
